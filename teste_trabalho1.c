7ce36598a521cb5f0f2c36f56c4db45ca8118002
brucopi <brucopi12@gmail.com>
2015-09-11 11:47:50 -0300

    Coloquei um espaço

--- teste_trabalho1.c
+++ teste_trabalho1.c
@@ -1,13 +1,29 @@
-*
+ /*
  * Programa para testar o trabalho 1 sobre organização de camisetas
  */
 
 #include<stdio.h>
 #include<stdlib.h>
+#include <string.h>
 
 int main(){
 
 
-
+	int numCores, quantCamisetas;
+	char cor[20];
+	
+	printf("Digite o número de cores\n");//opcional
+	scanf("%d", &numCores);
+	while (numCores != 0){
+	
+		for(; numCores > 0; numCores--) 
+		{
+			scanf("%s", cor);
+			printf("%s\n ",cor);
+		}
+	}
+	
+	
+  
 return 0;
 }
